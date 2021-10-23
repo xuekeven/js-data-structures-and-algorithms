a8a926fa407e9bd1fcf4298cb35fab728634a6b3
Keven <kevenchn@qq.com>
2021-10-23 22:22:06 +0800

    new

--- Topics/大数相加.js
+++ Topics/大数相加.js
@@ -2,28 +2,28 @@
 
 // 大数相加：以字符串的形式读入两个较大的数字，编写一个函数计算它们的和，以字符串形式返回。
 function solveMeFirst1(number1, number2) {
-  let longArr, shortArr;
+  let longStr, shortStr;
   if (number1.length > number2.length) {
-    longArr = Array.from(number1);
-    shortArr = Array.from(number2);
+    longStr = number1;
+    shortStr = number2;
   } else {
-    longArr = Array.from(number2);
-    shortArr = Array.from(number1);
+    longStr = number2;
+    shortStr = number1;
   }
 
-  const max = longArr.length, min = shortArr.length;
+  const max = longStr.length, min = shortStr.length;
   let add10 = 0, res = '', num, num1, num2;
   for (let i = 0; i < max; i++) {
     if (i < min) {
-      num1 = Number(longArr[longArr.length - i - 1]);
-      num2 = Number(shortArr[shortArr.length - i - 1]);
+      num1 = Number(longStr[longStr.length - i - 1]);
+      num2 = Number(shortStr[shortStr.length - i - 1]);
     } else {
-      num1 = Number(longArr[longArr.length - i - 1]);
+      num1 = Number(longStr[longStr.length - i - 1]);
       num2 = 0;
     }
     num = num1 + num2 + add10;
     add10 = 0;
-    if ( i !== max - 1 && num >= 10) {
+    if (num >= 10 && i !== max - 1) {
       num = num - 10;
       add10 = 1;
     } 
@@ -32,4 +32,4 @@ function solveMeFirst1(number1, number2) {
   return res;
 }
 
-console.log(solveMeFirst1('589465849568', '669784652318'), 589465849568 + 669784652318);
+console.log(solveMeFirst1('589465849568234523', '66978465231823456'), 589465849568234523 + 66978465231823456);
